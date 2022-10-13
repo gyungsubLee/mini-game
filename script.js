ba6927686f8976924db54b5bbe11022b3b07b0f0
gyungsubLee <dltjq2323@gmail.com>
2022-10-13 23:08:20 +0900

    feat : Bubble
    -Add class/Bubble,update(),draw()
    -Add Array/bubblesArray
    -Add handleBubbles()

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ class Player {
     const dx = this.x - mouse.x;
     const dy = this.y - mouse.y;
     if (mouse.x != this.x) {
-      this.x -= dx / 30;
+      this.x -= dx /30;
     }
     if (mouse.y != this.y) {
-      this.y -= dy / 30;
+      this.y -= dy /30;
     }
   }
   draw() {
@@ -71,12 +71,47 @@ class Player {
 const player = new Player();
 
 // Bubbles
+const bubblesArray = [];
+class Bubble {
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = canvas.height + Math.random() * canvas.height;
+        this.radius = 50;
+        this.speed = Math.random() * 5 + 1;
+        this.distance;
+    }
+    update(){
+        this.y -= this.speed;
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+function handleBubbles(){
+    if (gameFrame % 50 == 0){
+        bubblesArray.push(new Bubble());
+        console.log(bubblesArray.length);
+    }
+    
+    bubblesArray.forEach((bubble)=>{
+        bubble.update();
+        bubble.draw();
+    })
+};
+
 
 // Animation Loop
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleBubbles();
   player.update();
   player.draw();
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
